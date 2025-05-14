de899363010381bbe17158ee7578c590314029c4
LinusWeb <linooow@gmail.com>
2025-05-14 15:56:18 -0300

    Config bd neon

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -5,8 +5,12 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 module.exports = pool;
